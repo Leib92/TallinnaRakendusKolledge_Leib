fe66226ca5fb1f1142e3ad91938c3ae2694f6358
Leib92 <allan.leibenau92@gmail.com>
2025-09-04 15:03:33 +0300

    - Added ConnectionStrings to appsettings - Added to Data: DbInitializer, SchoolContext - Successful Migration - Database Update

---
+++ TallinnaRakenduslikKolledge_Leib/Data/DbInitializer.cs
@@ -0,0 +1,27 @@
+﻿using TallinnaRakenduslikKolledge_Leib.Models;
+
+namespace TallinnaRakenduslikKolledge_Leib.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+            if (context.Students.Any())
+            {
+                return;
+            }
+
+            var students = new Student[]
+            {
+                new Student 
+                {
+                    FirstName = "George",
+                    LastName = "Teemus",
+                    EnrollmentDate = DateTime.Now,
+
+                }
+            };
+        }
+    }
+}
--- TallinnaRakenduslikKolledge_Leib/Program.cs
+++ TallinnaRakenduslikKolledge_Leib/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledge_Leib.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SchoolContext>(options => options.UseSqlServer
+    (builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
