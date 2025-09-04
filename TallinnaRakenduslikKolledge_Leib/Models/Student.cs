00ef203c7e5d68cee51ea8242cbd9e86757b6d02
Opilane TTHK <allan.leibenau92@gmail.com>
2025-09-04 12:58:35 +0300

    commit stuff
    fingers crossed nothing blows up

---
+++ TallinnaRakenduslikKolledge_Leib/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallinnaRakenduslikKolledge_Leib.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment>? Enrollments { get; set; }
+
+        /* My own stuff */
+
+        public string StudentGroup { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
