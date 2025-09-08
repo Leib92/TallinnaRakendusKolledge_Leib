9cbb56b17bff5f3812409edea3b91b66be909071
Leib92 <opilane@HP-CZC321705Y>
2025-09-08 14:50:15 +0300

    Continued with Students - Added Create View - Modified Index View - Fixed up Student Model and Controller - Added Migration + Updated Database

--- TallinnaRakenduslikKolledge_Leib/Controllers/StudentsController.cs
+++ TallinnaRakenduslikKolledge_Leib/Controllers/StudentsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TallinnaRakenduslikKolledge_Leib.Data;
+using TallinnaRakenduslikKolledge_Leib.Models;
 
 namespace TallinnaRakenduslikKolledge_Leib.Controllers
 {
@@ -14,6 +15,25 @@ namespace TallinnaRakenduslikKolledge_Leib.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Students.ToListAsync());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind
+            ("ID,LastName,FirstName,EnrollmentDate,Email")] Student student)
+        {
+            if (ModelState.IsValid) 
+            {
+                _context.Students.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index"); // Alternative: "return RedirectToAction(nameof(Index))"
+
+            }
+            return View(student);
         }
     }
 }
--- TallinnaRakenduslikKolledge_Leib/Models/Student.cs
+++ TallinnaRakenduslikKolledge_Leib/Models/Student.cs
@@ -13,9 +13,9 @@ namespace TallinnaRakenduslikKolledge_Leib.Models
 
         /* My own stuff */
 
-        public string StudentGroup { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? StudentGroup { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
 
     }
 }
