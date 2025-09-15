25a6fb50d712d4b55c58ce27bf07d48541a7fa21
Leib92 <opilane@HP-CZC321705Y>
2025-09-15 14:59:27 +0300

    Additional Stuff - Patched up InstructorController - Added Index into Instructors View

--- TallinnaRakenduslikKolledge_Leib/Controllers/InstructorController.cs
+++ TallinnaRakenduslikKolledge_Leib/Controllers/InstructorController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledge_Leib.Data;
+using TallinnaRakenduslikKolledge_Leib.Models;
 
 namespace TallinnaRakenduslikKolledge_Leib.Controllers
 {
     public class InstructorController : Controller
     {
-        public IActionResult Index()
+        private readonly SchoolContext _context;
+        public InstructorController(SchoolContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int? id, int? courseId)
+        {
+            var vm = new InstructorIndexData();
+            vm.Instructors = await _context.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.CourseAssignments)
+                .ToListAsync();
+            return View(vm);
         }
     }
 }
