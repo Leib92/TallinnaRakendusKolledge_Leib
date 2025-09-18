cbaa5521fefd6dec08a8b98a969b0c238dd58285
Leib92 <opilane@HP-CZC321705Y>
2025-09-18 12:00:08 +0300

    Continuing with Improving Instructors - Added AssignedCourseData in Models - Modified InstructorsController to add Create + PopulateAssignedCourseData

--- TallinnaRakenduslikKolledge_Leib/Controllers/InstructorsController.cs
+++ TallinnaRakenduslikKolledge_Leib/Controllers/InstructorsController.cs
@@ -22,5 +22,58 @@ namespace TallinnaRakenduslikKolledge_Leib.Controllers
                 .ToListAsync();
             return View(vm);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var instructor = new Instructor();
+            instructor.CourseAssignments = new List<CourseAssignment>();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Instructor instructor, string selectedCourses)
+        {
+            if (selectedCourses == null)
+            {
+                instructor.CourseAssignments = new List<CourseAssignment>();
+                foreach (var course in selectedCourses)
+                {
+                    var courseToAdd = new CourseAssignment
+                    {
+                        InstructorId = instructor.Id,
+                        CourseId = course
+                    };
+                    instructor.CourseAssignments.Add(courseToAdd);
+                }
+                
+            }
+            if (ModelState.IsValid) 
+            {
+                _context.Add(instructor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            PopulateAssignedCourseData(instructor);
+            return View(instructor);
+        }
+        private void PopulateAssignedCourseData(Instructor instructor) 
+        {
+            var allCourses = _context.Courses; // leiame kõik kursused
+            var instructorCourses = new HashSet<int>(instructor.CourseAssignments.Select(c => c.CourseId));
+            // valime kursused kus courseid on õpetajal olemas
+            var vm = new List<AssignedCourseData>();
+            foreach (var course in allCourses) 
+            {
+                vm.Add(new AssignedCourseData
+                {
+                    CourseId = course.CourseId,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseId)
+                });
+            }
+            ViewData["Courses"] = vm;
+        }
     }
 }
